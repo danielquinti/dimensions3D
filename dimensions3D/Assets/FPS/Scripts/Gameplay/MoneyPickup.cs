85f406b4e1d92892c75ac257cdf18e2b1afac1de
danielquinti <daniel.quintillan@udc.es>
2021-04-28 14:18:08 +0200

    stable geardrop

---
+++ dimensions3D/Assets/FPS/Scripts/Gameplay/MoneyPickup.cs
@@ -0,0 +1,15 @@
+﻿using Unity.FPS.Game;
+using UnityEngine;
+
+namespace Unity.FPS.Gameplay
+{
+    public class MoneyPickup : Pickup
+    {
+
+        protected override void OnPicked(PlayerCharacterController player)
+        {
+            PlayPickupFeedback();
+            Destroy(gameObject);
+        }
+    }
+}
