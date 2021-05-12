b827d24c27261440ebedcf26353ae2e88da60ff7
Ivan <ivan.garcia.fernandez@udc.es>
2021-05-12 11:54:33 +0200

    Implement DeactivateTrap with Event

--- dimensions3D/Assets/Dimensions/Scripts/VendingDoor.cs
+++ dimensions3D/Assets/Dimensions/Scripts/VendingDoor.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.FPS.Gameplay;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class VendingDoor : Vending
 {
     public Animator doorAnimator;
     public RoomManager manager;
     private bool active = true;
+    public UnityEvent saleEvent;
+    
     override public void ShowPrice()
     {
         if (active) { display.SetInfo("Press F to open for " + price + " tokens."); }
@@ -16,7 +19,10 @@ public class VendingDoor : Vending
     {
         display.SetInfo("Door opened.");
         base.Sell(player);
-        manager.OnEnterRoom();
+        if (saleEvent == null)
+            manager.OnEnterRoom();
+        else
+            saleEvent.Invoke();
         Disable();
     }
 
